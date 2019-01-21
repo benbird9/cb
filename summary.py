caea838cea3fc4cd2c6ecea9db5db209ad06d248
qi.yu <qi.yu@thomsonreuters.com>
2019-01-21 17:05:36 +0800

    20190121 eod

--- summary.py
+++ summary.py
@@ -1,7 +1,8 @@
 count = 0
+TOTAL_BONDS = 18
 with open('tradehistory.txt', 'r') as f:
     for line in f:
         words = line.split()
         count = count + float(words[5]) - float(words[2])
 
-print count/18
+print count/TOTAL_BONDS
