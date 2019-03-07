9fcac01e5efba841c509ad31aaa9d225c2caaa6c
qi.yu <qi.yu@thomsonreuters.com>
2019-03-07 16:46:52 +0800

    20190307

--- JslDowloader.py
+++ JslDowloader.py
@@ -4,7 +4,7 @@ import pandas as pd
 import requests
 import time
 # import json
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 exclude_bonds = []
 SELECT_RANGE = 30
 TOP_RANGE = 18
@@ -75,9 +75,9 @@ exist_df.loc['TOTAL','ytm_rt'] = exist_df.ytm_rt.mean()
 exist_df.loc['TOTAL','sincrease_rt'] = exist_df.sincrease_rt.mean()
 exist_df.to_csv('jsl.existing.18.csv', encoding='utf-8')
 
-target_bonds = top18_defence
-sell_bonds = exist_bonds - top18_defence
-buy_bonds = top18_defence - exist_bonds
+target_bonds = top18_attack
+sell_bonds = exist_bonds - top18_attack
+buy_bonds = top18_attack - exist_bonds
 sell_df = rdf.loc[rdf.bond_id.isin(sell_bonds), :]
 buy_df = rdf.loc[rdf.bond_id.isin(buy_bonds), :]
 
