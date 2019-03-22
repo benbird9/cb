fb103a891bba22120e99a9960bb9dbc47fc16fc5
qi.yu <qi.yu@thomsonreuters.com>
2019-03-22 10:35:54 +0800

    change the py code

--- JslDowloader.py
+++ JslDowloader.py
@@ -75,9 +75,10 @@ exist_df.loc['TOTAL','ytm_rt'] = exist_df.ytm_rt.mean()
 exist_df.loc['TOTAL','sincrease_rt'] = exist_df.sincrease_rt.mean()
 exist_df.to_csv('jsl.existing.18.csv', encoding='utf-8')
 
-target_bonds = top18_attack
-sell_bonds = exist_bonds - top18_attack
-buy_bonds = top18_attack - exist_bonds
+# target_bonds = top18_attack
+target_bonds = top18_defence
+sell_bonds = exist_bonds - target_bonds
+buy_bonds = target_bonds - exist_bonds
 sell_df = rdf.loc[rdf.bond_id.isin(sell_bonds), :]
 buy_df = rdf.loc[rdf.bond_id.isin(buy_bonds), :]
 
