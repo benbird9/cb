b14ab394d198f20ff4e0e358228870d4cfe40130
qi.yu <qi.yu@thomsonreuters.com>
2019-04-17 15:11:05 +0800

    2019-04-17

--- submit.py
+++ submit.py
@@ -9,5 +9,5 @@ os.system('python SortBySumOfPrinceAndPremium.py')
 repo = git.Repo('\\a_yu_workdir\project\sourcecode\cb')
 git = repo.git
 git.execute('git add -A')
-git.execute("git commit -m '" + TODAY +"'")
+git.execute("git commit -m " + TODAY)
 git.execute('git push')
