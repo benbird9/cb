6301c5d97549319c8eb24b4208f4d3b6291c7b7f
qi.yu <qi.yu@thomsonreuters.com>
2019-04-20 21:48:35 +0800

    add 蓝标板砖 indicator

--- tushareTrial.py
+++ tushareTrial.py
@@ -1,7 +1,7 @@
 import tushare as ts
 
 stock = ts.get_realtime_quotes('300058')
-history = ts.get_today_ticks('300058')
+bond = ts.get_realtime_quotes('123001')
 
-print stock
-print history
+print (float(stock.loc[0, 'bid'])*23.2 > float(bond.loc[0, 'a1_p']) * 1.005) & (float(bond.loc[0, 'a1_v']) > 4.3)
+
