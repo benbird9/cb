888f1b78c9ae9b21e840c79abb5b8efd719db766
qi.yu <qi.yu@thomsonreuters.com>
2019-04-22 12:58:05 +0800

    reconstruct

--- SortBySumOfPrinceAndPremium.py
+++ SortBySumOfPrinceAndPremium.py
@@ -62,7 +62,7 @@ replica_rdf = rdf.copy()
 replica_rdf.loc['TOTAL', 'price'] = replica_rdf.price.mean()
 replica_rdf.loc['TOTAL', 'premium_rt'] = replica_rdf.premium_rt.mean()
 
-with open('existing_bonds.txt') as f:
+with open('trade/existing_bonds.txt') as f:
     exist_bonds = f.readlines()
 exist_bonds = set([x.strip() for x in exist_bonds])
 exist_df = rdf.loc[rdf.bond_id.isin(exist_bonds), :]
@@ -71,10 +71,10 @@ exist_df.loc['TOTAL', 'premium_rt'] = exist_df.premium_rt.mean()
 exist_df.loc['TOTAL','ytm_rt'] = exist_df.ytm_rt.mean()
 exist_df.loc['TOTAL','sincrease_rt'] = exist_df.sincrease_rt.mean()
 
-replica_rdf.to_csv('jsl.rlst.csv', encoding='utf-8')
-rdf18_attack.to_csv('jsl.attack.18.csv', encoding='utf-8')
-rdf18_defence.to_csv('jsl.defence.18.csv', encoding='utf-8')
-exist_df.to_csv('jsl.existing.18.csv', encoding='utf-8')
+replica_rdf.to_csv('output/jsl.rlst.csv', encoding='utf-8')
+rdf18_attack.to_csv('output/jsl.attack.18.csv', encoding='utf-8')
+rdf18_defence.to_csv('output/jsl.defence.18.csv', encoding='utf-8')
+exist_df.to_csv('output/jsl.existing.18.csv', encoding='utf-8')
 #save to data folder
 replica_rdf.to_csv('data/' + TODAY + '.jsl.rlst.csv', encoding='utf-8')
 rdf18_attack.to_csv('data/' + TODAY + '.jsl.attack.18.csv', encoding='utf-8')
@@ -103,12 +103,12 @@ special_rdf = rdf.loc[~rdf.convert_cd.str.startswith('1'), ['price', 'premium_rt
 plt.plot(rdf['price'], rdf['premium_rt'], 'b+')
 plt.plot(special_rdf['price'], special_rdf['premium_rt'], 'bx')
 plt.plot(exist_df['price'], exist_df['premium_rt'], 'ro')
-plt.axis([80, 150, -10, 60])
+plt.axis([80, 200, -20, 60])
 plt.title(datetime.now().strftime("%Y-%m-%d %I:%M:%S"))
 plt.xlabel('price')
 plt.ylabel('premium_ratio')
 plt.grid(True)
-plt.savefig('pic/'+ TODAY + '.png')
+plt.savefig('pic/'+ TODAY + '.png', dpi=300)
 
 
 
--- tushareTrial.py
+++ tushareTrial.py
@@ -1,7 +1,28 @@
 import tushare as ts
+import pandas as pd
 
-stock = ts.get_realtime_quotes('300058')
-bond = ts.get_realtime_quotes('123001')
 
-print (float(stock.loc[0, 'bid'])*23.2 > float(bond.loc[0, 'a1_p']) * 1.005) & (float(bond.loc[0, 'a1_v']) > 4.3)
 
+def FloatOrZero(value):
+    try:
+        return float(value)
+    except:
+        return 0.0
+def printGrid():
+    stock = ts.get_realtime_quotes('300058')
+    bond = ts.get_realtime_quotes('123001')
+    df = pd.DataFrame(columns=['b_benefit', 'b_vol', 'b_price','level', 'a_price', 'a_vol', 'a_benefit'])
+    stock_price = float(stock.loc[0, 'bid'])*23.2
+    for i in range(1, 6):
+        bid_price = FloatOrZero(bond.loc[0, 'b'+str(i)+'_p'])
+        bid_volume = FloatOrZero(bond.loc[0, 'b'+str(i)+'_v'])*10
+        ask_price = FloatOrZero(bond.loc[0, 'a'+str(i)+'_p'])
+        ask_volume = FloatOrZero(bond.loc[0, 'a'+str(i)+'_v'])*10
+        df.loc[i] = [(stock_price/bid_price -1)*100 ,bid_volume, bid_price, i, ask_price, ask_volume, (stock_price/ask_price -1)*100]
+    print df
+    print '\n\n'
+
+import time
+while True:
+    printGrid()
+    time.sleep(5)
