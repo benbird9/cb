89aec691a7a6598f06ffe95fdccd76db9c42a24c
qi.yu <qi.yu@thomsonreuters.com>
2019-04-22 15:40:57 +0800

    2019-04-22

---
+++ Print2.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+DATE = '2019-04-19'
+FULL_DATA_FILE_PATH = './data/'+DATE+'.jsl.rlst.csv'
+PARTIAL_FILE_PATH = './data/'+DATE+'.jsl.existing.18.csv'    # only read bond_id from the file
+
+rdf = pd.read_csv(FULL_DATA_FILE_PATH, dtype={'bond_id': object}, encoding='utf-8', index_col=0)
+rdf.drop(rdf.tail(1).index, inplace=True)
+
+partial_csv_df = pd.read_csv(PARTIAL_FILE_PATH, dtype={'bond_id': object}, encoding='utf-8', index_col=0)
+partial_csv_df.drop(partial_csv_df.tail(1).index, inplace=True)
+
+
+special_rdf = rdf.loc[~rdf.convert_cd.str.startswith('1'), ['price', 'premium_rt']]
+plt.plot(rdf['price'], rdf['premium_rt'], 'b+')
+plt.plot(special_rdf['price'], special_rdf['premium_rt'], 'bx')
+plt.plot(partial_csv_df['price'], partial_csv_df['premium_rt'], 'ro')
+plt.axis([80, 200, -20, 60])
+plt.title(DATE + ' 03:40:21')
+plt.xlabel('price')
+plt.ylabel('premium_ratio')
+plt.grid(True)
+# plt.savefig('pic/'+ TODAY + '.png', dpi=300)
+plt.savefig('pic/'+ DATE + '.png', dpi=150)
---
+++ lanbiao300058.py
@@ -0,0 +1,54 @@
+import tushare as ts
+import pandas as pd
+
+
+
+def FloatOrZero(value):
+    try:
+        return float(value)
+    except:
+        return 0.0
+def printGrid():
+    stock = ts.get_realtime_quotes('300058')
+    bond = ts.get_realtime_quotes('123001')
+    df = pd.DataFrame(columns=['b_benefit', 'b_vol', 'b_price','level', 'a_price', 'a_vol', 'a_benefit'])
+    stock_price = float(stock.loc[0, 'bid'])*23.2
+    for i in range(1, 6):
+        bid_price = FloatOrZero(bond.loc[0, 'b'+str(i)+'_p'])
+        bid_volume = FloatOrZero(bond.loc[0, 'b'+str(i)+'_v'])*10
+        ask_price = FloatOrZero(bond.loc[0, 'a'+str(i)+'_p'])
+        ask_volume = FloatOrZero(bond.loc[0, 'a'+str(i)+'_v'])*10
+        df.loc[i] = [(stock_price/bid_price -1)*100 ,bid_volume, bid_price, i, ask_price, ask_volume, (stock_price/ask_price -1)*100]
+    print df
+    print 'stock price:' + str(float(stock.loc[0, 'bid']))
+    print '\n\n'
+    return df
+
+def send_email(df):
+    import smtplib
+    from email.mime.text import MIMEText
+    _user = "10314731@qq.com"
+    _pwd = "liyekxqicqntcacj"
+    _to = "10314731@qq.com"
+
+    msg = MIMEText(df.to_string())
+    msg["Subject"] = "300058"
+    msg["From"] = _user
+    msg["To"] = _to
+
+    try:
+        s = smtplib.SMTP_SSL("smtp.qq.com", 465)
+        s.login(_user, _pwd)
+        s.sendmail(_user, _to, msg.as_string())
+        s.quit()
+        print "Success!"
+    except smtplib.SMTPException, e:
+        print "Falied,%s" % e
+
+import time
+while True:
+    df = printGrid()
+    if df.loc[1, 'a_benefit'] > 0.8:
+        send_email(df)
+        break
+    time.sleep(5)
