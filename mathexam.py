860ff9ad97e5470325ee4ac3bba61efa3dbfa1c9
qi.yu <qi.yu@thomsonreuters.com>
2019-04-23 17:41:21 +0800

    math exam

--- mathexam.py
+++ mathexam.py
@@ -3,21 +3,27 @@ import random
 print "Welcome Yu Lanqing for math exam today!! \n"
 raw_input("Press any key to start....")
 
-
+error = 0
 for x in range(0, 80):
     x = random.randint(1, 9)
     y = random.randint(1, 9)
     mul_value = x * y
-    z = random.randint(1, 99)
+    z = random.randint(1, 99 - mul_value)
 
     expr = ''
     if z % 4 == 0:
-        # multiply
-        if z + mul_value > 100:
-            expr = ''
-        else:
-            expr =
-    else if
+        expr = str(z) + '+' + str(x) + '*' + str(y)
+    elif z % 4 == 1:
+        expr = str(z) + '+' + str(mul_value) + '/' + str(x)
+    elif z % 4 == 2:
+        expr = str(z+x*y) + '-' + str(x) + '*' + str(y)
+    elif z % 4 == 3:
+        expr = str(z+x*y) + '-' + str(mul_value) + '/' + str(x)
 
-    var = raw_input("Please enter something: ")
-    print var
+    var = int(raw_input(expr + '='))
+    if var == eval(expr):
+        print 'Good Job, :) '
+    else:
+        print "Wrong, :("
+        error = error + 1
+print 'total error:' + str(error)
