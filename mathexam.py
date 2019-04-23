b9e29b6d89f1bc7816187658cd33c89294b3fa91
qi.yu <qi.yu@thomsonreuters.com>
2019-04-23 15:40:22 +0800

    2019-04-23

---
+++ mathexam.py
@@ -0,0 +1,23 @@
+import random
+
+print "Welcome Yu Lanqing for math exam today!! \n"
+raw_input("Press any key to start....")
+
+
+for x in range(0, 80):
+    x = random.randint(1, 9)
+    y = random.randint(1, 9)
+    mul_value = x * y
+    z = random.randint(1, 99)
+
+    expr = ''
+    if z % 4 == 0:
+        # multiply
+        if z + mul_value > 100:
+            expr = ''
+        else:
+            expr =
+    else if
+
+    var = raw_input("Please enter something: ")
+    print var
--- summary.py
+++ summary.py
@@ -1,6 +1,6 @@
 count = 0
 TOTAL_BONDS = 18
-with open('tradehistory.txt', 'r') as f:
+with open('trade\\tradehistory.txt', 'r') as f:
     for line in f:
         words = line.split()
         count = count + float(words[5]) - float(words[2])
