6a67bb282cbc834eb8ec59cb557e05eb97482138
qi.yu <qi.yu@thomsonreuters.com>
2019-04-29 13:55:17 +0800

    rename some filenames

--- submit.py
+++ submit.py
@@ -3,11 +3,14 @@ from datetime import datetime
 import os
 TODAY = datetime.today().strftime('%Y-%m-%d')
 
+repo = git.Repo('\\a_yu_workdir\project\sourcecode\cb')
+git = repo.git
+git.execute('git fetch origin master')
+git.execute('git reset --hard origin/master')
+
 os.chdir('\\a_yu_workdir\project\sourcecode\cb')
 os.system('python SortBySumOfPrinceAndPremium.py')
 
-repo = git.Repo('\\a_yu_workdir\project\sourcecode\cb')
-git = repo.git
 git.execute('git add -A')
 git.execute("git commit -m " + TODAY)
 git.execute('git push')
