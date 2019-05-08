1bbde9738eeefc9b5d889cfb8948156225cfb733
qi.yu <qi.yu@thomsonreuters.com>
2019-05-08 16:53:48 +0800

    定投中证500结果

--- csi500.py
+++ csi500.py
@@ -1,7 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import datetime
 
 csv = pd.read_csv('./data/csi500.csv')
-csv.Close.min()
-plt.plot(csv.Date, csv.Close, 'ro')
-plt.show()
+csv.Date = csv.Date.apply(lambda s: datetime.datetime.strptime(s, "%Y/%m/%d").date())
+
+past_3_years_df = csv.loc[csv.Date > datetime.datetime(2016, 5, 17).date(), :]
+
+list = past_3_years_df.Close.tolist()
+for i in range(245):
+    list.append(4000.0)
+print list
+list_b = [(lambda x: 1/x)(i) for i in list]
+print list_b
+
+
+print len(list_b)/sum(list_b)
+print sum(list)/len(list)
