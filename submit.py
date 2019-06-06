39564888882e2cf52f3ae4c1dd2fdf9612b8f58a
qi.yu <qi.yu@thomsonreuters.com>
2019-06-06 10:10:00 +0800

    2019-06-06 sell:     bond_id bond_nm increase_rt    price  sincrease_rt  premium_rt adj_scnt  ytm_rt rating_cd  year_left curr_iss_amt convert_cd     sum 33   123023    迪森转债      -0.25%   98.010         -0.81       15.52        0    2.90        AA      5.792        6.000      未到转股期  86.470 47   113524    奇精转债      -0.65%   97.150         -4.42       18.87        0    3.70        AA      5.529        3.300      未到转股期  83.980 48   128033    迪龙转债      -0.32%   97.800         -0.94       18.34        1    2.72        AA      4.562        5.199     128033  83.860 53   113528    长城转债      -0.01%   97.580         -2.77       19.33        0    3.46       AA-      5.740        6.340      未到转股期  83.090 54   127003    海印转债      -0.50%  100.600         -0.76       16.34        2    4.44        AA      3.008       11.108     127003  83.060 60   123011    德尔转债      -0.32%   95.500         -1.76       22.20        0    4.25       AA-      5.121        5.556     123011  82.300 77   123019    中来转债      -0.34%   99.160         -0.18       20.75        0    4.21       AA-      5.729       10.000      未到转股期  80.090 78   128044    岭南转债      -0.02%   98.490         -0.62       21.98        1    2.58        AA      5.195        6.594     128044  79.530 84   123024    岱勒转债      -0.46%   96.161         -0.52       25.49        0    3.54        A+      4.795        2.100      未到转股期  78.349 99   128018    时达转债       0.00%   94.901          0.00       29.49        1    3.64        AA      4.422        8.823     128018  75.609 105  113016    小康转债      -0.08%   94.360         -2.94       33.03        1    3.78        AA      4.422        8.814     191016  72.610 buy:    bond_id bond_nm increase_rt    price  sincrease_rt  premium_rt adj_scnt  ytm_rt rating_cd  year_left curr_iss_amt convert_cd     sum 1   113535    大业转债       0.37%  101.920          0.55        0.64        0    2.24        AA      4.926        5.000      未到转股期  97.440 2   128057    博彦转债       0.09%  102.188          0.23        3.13        0    2.53       AA-      5.751        5.758      未到转股期  94.682 6   128034    江银转债      -0.03%  102.650         -0.22        4.21        2    1.44       AA+      4.644       17.583     128034  93.140 8   113025    明泰转债      -0.25%   96.820         -0.40       10.59        0    3.12        AA      5.847       18.390      未到转股期  92.590 10  128049    华源转债      -0.72%  101.035         -0.84        7.16        0    2.56       AA-      5.482        4.000     128049  91.805 13  113519    长久转债      -1.03%  102.380         -4.51        6.05        0    3.13        AA      5.427        6.969     191519  91.570 16  123012    万顺转债      -0.89%  102.100         -0.39        6.68        0    1.74       AA-      5.126        1.703     123012  91.220 17  113527    维格转债      -0.27%   97.240          0.00       11.55        0    3.99        AA      5.638        7.460      未到转股期  91.210 18  128048    张行转债      -0.24%  101.054          0.00        7.81        0    2.34       AA+      5.441       24.998     128048  91.136 24  123014    凯发转债       0.51%  100.800         -2.23       10.27        0    2.05        A+      4.142        3.150     123014  88.930 26  128063    未来转债      -0.14%  103.210         -1.68        9.11        0    3.96       AA-      5.830        6.300      未到转股期  87.680 29  127012    招路转债      -0.11%  100.276         -0.84       12.83        0    1.35       AAA      5.795       50.000      未到转股期  86.894

--- submit.py
+++ submit.py
@@ -8,9 +8,12 @@ git = repo.git
 # git.execute('git fetch origin master')
 # git.execute('git reset --hard origin/master')
 
-os.chdir('\\a_yu_workdir\project\sourcecode\cb')
-os.system('python SortBySumOfPrinceAndPremium.py')
+# os.chdir('\\a_yu_workdir\project\sourcecode\cb')
+# os.system('python SortBySumOfPrinceAndPremium.py')
+import subprocess
+result = subprocess.check_output('python SortBySumOfPrinceAndPremium.py', shell=True, cwd='\\a_yu_workdir\project\sourcecode\cb')
+
 
 git.execute('git add -A')
-git.execute("git commit -m " + TODAY)
+git.execute('git commit -m "' + TODAY + ' ' + result + '"')
 git.execute('git push')
