099a4ff623b7e65d16cece64a1ec4cb0aa38c09d
Your Name <yuqi.mail@foxmail.com>
2020-08-01 00:04:22 +0800

    2020-08-01

--- cb.trade.summary.py
+++ cb.trade.summary.py
@@ -1,8 +1,8 @@
 count = 0
 TOTAL_BONDS = 18
-with open('trade\\cb.trade.history.txt', 'r') as f:
+with open('trade\\cb.trade.history.txt', 'r',encoding='gb18030', errors='ignore') as f:
     for line in f:
         words = line.split()
         count = count + float(words[5]) - float(words[2])
 
-print count/TOTAL_BONDS
+print(count/TOTAL_BONDS)
